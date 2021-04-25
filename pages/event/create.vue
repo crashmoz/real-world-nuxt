202aeef6704c42bca76e2a09c935082e59002658
Crashm <ismail.setiawan81@gmail.com>
2021-04-25 11:43:42 +0700

    try to deploy

---
+++ pages/event/create.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    // <-- property used by vue-meta to add header tags
+    return {
+      title: 'Event Listing - Real World Events', // for our title tag
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  head() {
+    return {
+      title: 'Event Listing',
+    }
+  },
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time',
+      })
+    }
+  },
+  computed: mapState({
+    events: (state) => state.events.events,
+  }),
+}
+</script>
